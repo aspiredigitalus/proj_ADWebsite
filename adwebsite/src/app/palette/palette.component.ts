7d5b787d1a08902d5d424f228af8c73c8995b2c1
davidmehoves-adus <david.mehoves@aspiredigital.com>
2023-01-19 20:47:13 -0600

    Palette Demonstration

---
+++ adwebsite/src/app/palette/palette.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'palette',
+  templateUrl: './palette.component.html',
+  styleUrls: ['./palette.component.css']
+})
+export class PaletteComponent {
+
+}
