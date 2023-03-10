4ff4a9719e47458be546527dbea2c82db4c50570
CoryMunselle-adus <122128963+CoryMunselle-adus@users.noreply.github.com>
2023-03-10 16:35:44 -0600

    mapped sap solutions and c4c pages to header

---
+++ adwebsite/src/app/cloud-for-customer/cloud-for-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cloud-for-customer',
+  templateUrl: './cloud-for-customer.component.html',
+  styleUrls: ['./cloud-for-customer.component.css']
+})
+export class CloudForCustomerComponent {
+
+}
