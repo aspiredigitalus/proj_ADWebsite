920c2cbdd4014f08da46339a1185c87bbdd5551d
davidmehoves-adus <david.mehoves@aspiredigital.com>
2023-04-09 18:10:40 -0500

    home-language

--- adwebsite/src/app/home/home.component.ts
+++ adwebsite/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../services/language/language-service.service';
 
 @Component({
   selector: 'home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+    languageService: LanguageService;
+    language:Map<string,string>;
+    
+    constructor(languageService: LanguageService){
+        this.language = new Map();
+        this.languageService = languageService;
+        this.languageService.languageMap.subscribe((lang:Map<string,string>)=>{
+            this.language = lang;
+        });
+    }
 }
