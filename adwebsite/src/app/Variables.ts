3c793d27ec6e98435a73c2b7e54e890cc1175b62
davidmehoves-adus <david.mehoves@aspiredigital.com>
2023-06-24 11:27:50 -0500

    redirect to https APIs

--- adwebsite/src/app/Variables.ts
+++ adwebsite/src/app/Variables.ts
@@ -1,4 +1,4 @@
 export class Variables {
    // static baseurl: string = "http://aspiredigital.asuscomm.com:8081/";
-   static baseurl: string = "localhost/"
+   static baseurl: string = "https://localhost:8443/"
 }
